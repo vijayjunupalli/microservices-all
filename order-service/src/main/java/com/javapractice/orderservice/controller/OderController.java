0ea5cfa532560f54825db6b2df7390d783fd564d
vijay <vijay.junupalli@gmail.com>
2024-02-01 23:21:07 -0600

    circuit braker changes using resilient implementation

--- order-service/src/main/java/com/javapractice/orderservice/controller/OderController.java
+++ order-service/src/main/java/com/javapractice/orderservice/controller/OderController.java
@@ -2,6 +2,7 @@ package com.javapractice.orderservice.controller;
 
 import com.javapractice.orderservice.dto.OrderRequest;
 import com.javapractice.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,15 @@ public class OderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order placed successfully ";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException exception){
+
+        return "Requests are failing! Please try after sometime ";
 
     }
 }
